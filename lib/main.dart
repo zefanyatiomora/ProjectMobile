efedfed1f49db4d6a14f51a47965e2aea148b3b5
zefanya <zefanyatiomora@gmail.com>
2024-09-29 15:24:00 +0700

    Tampilan Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:login_page/lupaKataSandi_page.dart'; 
+import 'package:login_page/login_page.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+void main() {
+  runApp(const MainApp());
+}
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login Page"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Input Fields dan Login Button
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-              ),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implementasi Login di sini
-              },
-              child: const Text("Login"),
-            ),
-            const SizedBox(height: 20),
-
-            // Teks atau tombol "Lupa Kata Sandi"
-            TextButton(
-              onPressed: () {
-                // Navigasi ke halaman lupa kata sandi
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ForgotPasswordPage(),
-                  ),
-                );
-              },
-              child: const Text("Lupa Kata Sandi?"),
-            ),
-          ],
-        ),
-      ),
+      home: const LoginPage(),
     );
   }
 }
